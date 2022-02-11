223fea4b6a110c5a75e0962e13d6ea917e0dbddd
Serhii Bura <serhiibura@gmail.com>
2022-02-11 20:21:30 +0200

    engine: Vector update
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

---
+++ engine/vector/vector_2d.cpp
@@ -0,0 +1,85 @@
+#include "vector_2d.hpp"
+
+Vector2D::Vector2D(float x, float y) 
+    : m_x(x), m_y(y)
+{};
+
+void Vector2D::setX(float x)
+{
+    m_x = x;
+};
+
+void Vector2D::setY(float y)
+{
+    m_y = y;
+};
+
+Vector2D& Vector2D::operator +=(Vector2D& vector)
+{
+    m_x += vector.m_x;
+    m_y += vector.m_y;
+
+    return *this;
+}
+
+Vector2D operator +(Vector2D& vector1, const Vector2D& vector2)
+{
+    return Vector2D(vector1.m_x + vector2.m_x, vector1.m_y + vector2.m_y);
+}
+
+Vector2D& Vector2D::operator -=(Vector2D& vector)
+{
+    m_x -= vector.m_x;
+    m_y -= vector.m_y;
+
+    return *this;
+}
+
+Vector2D operator -(Vector2D& vector1, const Vector2D& vector2)
+{
+    return Vector2D(vector1.m_x - vector2.m_x, vector1.m_y - vector2.m_y);
+}
+
+Vector2D& Vector2D::operator *=(Vector2D& vector)
+{
+    m_x *= vector.m_x;
+    m_y *= vector.m_y;
+
+    return *this;
+}
+
+Vector2D operator *(Vector2D& vector1, const Vector2D& vector2)
+{
+    return Vector2D(vector1.m_x * vector2.m_x, vector1.m_y * vector2.m_y);
+}
+
+Vector2D& Vector2D::operator*=(float scale)
+{
+    m_x *= scale;
+    m_y *= scale;
+
+    return *this;
+}
+
+Vector2D& Vector2D::operator/=(float scale)
+{
+    m_x /= scale;
+    m_y /= scale;
+
+    return *this;
+}
+
+float Vector2D::length()
+{
+    return std::sqrt(m_x * m_x + m_y * m_y);
+}
+
+float Vector2D::lengthSqrt()
+{
+    return std::sqrt(m_x * m_x + m_y * m_y);
+}
+
+Vector2D Vector2D::normalize()
+{
+    return Vector2D(m_x / length(), m_y / length());
+}
--- engine/vector/vector_2d.hpp
+++ engine/vector/vector_2d.hpp
@@ -5,57 +5,30 @@
 
 class Vector2D {
 public:
-    Vector2D(float x, float y) : m_x(x), m_y(y) {};
+    Vector2D(float x, float y);
 
-    void setX(float x) { m_x = x; };
-    void setY(float y) { m_y = y; };
+    void setX(float x);
+    void setY(float y);
 
     float getX() { return m_x; }
     float getY() { return m_y; }
 
-    Vector2D& operator +=(Vector2D& vector)
-    {
-        m_x += vector.m_x;
-        m_y += vector.m_y;
+    Vector2D& operator +=(Vector2D& vector);
+    friend Vector2D operator +(Vector2D& vector1, const Vector2D& vector2);
 
-        return *this;
-    }
+    Vector2D& operator -=(Vector2D& vector);
+    friend Vector2D operator -(Vector2D& vector1, const Vector2D& vector2);
 
-    friend Vector2D operator +(Vector2D& vector1, const Vector2D& vector2)
-    {
-        return Vector2D(vector1.m_x + vector2.m_x, vector1.m_y + vector2.m_y);
-    }
+    Vector2D& operator *=(Vector2D& vector);
+    friend Vector2D operator *(Vector2D& vector1, const Vector2D& vector2);
 
-    Vector2D& operator -=(Vector2D& vector)
-    {
-        m_x -= vector.m_x;
-        m_y -= vector.m_y;
+    Vector2D& operator *=(float scale);
+    Vector2D& operator /=(float scale);
 
-        return *this;
-    }
+    float length();
+    float lengthSqrt();
 
-    friend Vector2D operator -(Vector2D& vector1, const Vector2D& vector2)
-    {
-        return Vector2D(vector1.m_x - vector2.m_x, vector1.m_y - vector2.m_y);
-    }
-
-    Vector2D& operator *=(Vector2D& vector)
-    {
-        m_x *= vector.m_x;
-        m_y *= vector.m_y;
-
-        return *this;
-    }
-
-    friend Vector2D operator *(Vector2D& vector1, const Vector2D& vector2)
-    {
-        return Vector2D(vector1.m_x * vector2.m_x, vector1.m_y * vector2.m_y);
-    }
-
-    float length()
-    {
-        return std::sqrt(m_x * m_x + m_y * m_y);
-    }
+    Vector2D normalize();
 
 private:
     float m_x, m_y;
